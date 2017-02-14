cfd01117f33a5d244b947820fe11bdd4c6f40091
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-14 09:24:09 -0500

    Finished the java portion of this project. ready to start styling.

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function gather(callback){
+    $.ajax({
+    url: `http://json-data.herokuapp.com/forms/`,
+    dataType: "json",
+    success: callback
+  })
+}
+function getInfo(data) {
+  console.log(data)
+  for (i=0; i < data.length; i++){
+    if (data[i].type === "text" || data[i].type === "email" || data[i].type === "tel"){
+$(".content").append(`<div><i class = "fa ${data[i].icon}"></i>
+<input type= text "${data[i].label}" placeholder = "${data[i].label}" size = "50"></div>`)
+}
+  else if (data[i].type === "select"){
+    $(".content").append(`<div><select>
+      <option value = "Select Language...">Select Language...</option>
+      <option value = "English">English</option>"
+      <option value = "Spanish">Spanish</option>"
+      <option value = "French">French</option>"
+      <option value = "Japanese">Japanese</option>"`)
+      }
+      else if (data[i].type === "textarea"){
+        $(".content").append(`<div><i class = "fa ${data[i].icon}"></i><textarea rows="4" cols="50"> </textarea> </div>`)
+      }
+    }
+  }
+
+gather(getInfo)
