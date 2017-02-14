655af80d923000a609f2fb2f43f0fe575256a778
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-14 15:15:45 -0500

    Project is fully working

--- javascript.js
+++ javascript.js
@@ -13,12 +13,9 @@ $(".content").append(`<div><i class = "fa ${data[i].icon}"></i>
 <input type= text "${data[i].label}" placeholder = "${data[i].label}" size = "65"></div>`)
 }
   else if (data[i].type === "select"){
-    $(".content").append(`<div><select>
-      <option value = "Select Language...">Select Language...</option>
-      <option value = "English">English</option>"
-      <option value = "Spanish">Spanish</option>"
-      <option value = "French">French</option>"
-      <option value = "Japanese">Japanese</option>"</div>`)
+    $(".content").append(`<div><select><option value = "Select Language...">Select Language...</option>
+      ${optionLang(data[i])}
+      </select></div>`)
       }
       else if (data[i].type === "textarea"){
         $(".content").append(`<div><i class = "fa ${data[i].icon}"></i>
@@ -26,5 +23,13 @@ $(".content").append(`<div><i class = "fa ${data[i].icon}"></i>
       }
     }
   }
+var optionString = "";
+function optionLang (x) {
+  var optionString = "";
+  for (var count = 0; count < x.options.length; count++) {
+  optionString += `<option>${x.options[count].label}</option>"`
+ }
+ return optionString;
+}
 
 gather(getInfo)
